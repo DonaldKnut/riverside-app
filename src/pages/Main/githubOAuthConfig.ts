9bb4f76a4784c7483a34fda14cf6eb0be79260af
DonaldKnut <openiyiibrahim@gmail.com>
2023-12-04 10:06:51 +0100

    latest

--- src/pages/Main/githubOAuthConfig.ts
+++ src/pages/Main/githubOAuthConfig.ts
@@ -2,5 +2,5 @@
 
 export const githubOAuthConfig = {
   clientId: "39d5feed66a8d65723a8",
-  redirectUri: "https://riverside-victory.netlify.app/dashboard",
+  redirectUri: "https://riverside-victory.netlify.app/auth/dashboard",
 };
